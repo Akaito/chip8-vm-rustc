acbdfe0b6de7df81690a065e26caf42c0c958ec0
Chris Barrett <chris@codesaru.com>
2020-06-29 23:06:32 -0700

    Many tweaks/changes.
    Shift things around to make rustc happier.
    
    Have to learn a lot more about borrowing and structs.

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,64 @@
 // Which itself is *heavily* based on Laurence Muller's tutorial at
 // http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
 
+
+const MEMORY_BYTES:   usize = 4096;
+const REGISTER_COUNT: usize = 16;
+const RENDER_WIDTH:   usize = 64;
+const RENDER_HEIGHT:  usize = 32;
+const STACK_SIZE:     usize = 16;
+const KEY_COUNT:      usize = 16;
+
+const INTERPRETER_BEGIN:  u16 = 0x000;
+const INTERPRETER_END:    u16 = 0x1FF;
+const FONT_BEGIN:         u16 = 0x050;
+const FONT_END:           u16 = 0x0A0;
+const PROG_ROM_RAM_BEGIN: u16 = 0x200;
+const PROG_ROM_RAM_END:   u16 = 0xFFF;
+
+
+#[derive(Debug)]
 struct Chip8 {
-    static const MEMORY_BYTES:   u32 = 4096;
-    static const REGISTER_COUNT: u32 = 16;
-    static const RENDER_WIDTH:   u32 = 64;
-    static const RENDER_HEIGHT:  u32 = 32;
-    static const STACK_SIZE:     u32 = 16;
-    static const KEY_COUNT:      u32 = 16;
+    // reminder: 'static' isn't a thing like this
+    memory: Vec<u8>,               // should have MEMORY_BYTES elements
+    v:      [u8; REGISTER_COUNT],  // registers V1-VF
+    i:           u16,              // index register
+    pc:          u16,              // program counter
+    delay_timer: u8,               // decrement if not 0
+    sound_timer: u8,               // decrement if not 0, beep on reaching 0
+    key_states:  [u8; KEY_COUNT],  // hex keypad buttonstates
 
-    static const INTERPRETER_BEGIN:  u16 = 0x000;
-    static const INTERPRETER_END:    u16 = 0x1FF;
-    static const FONT_BEGIN:         u16 = 0x050;
-    static const FONT_END:           u16 = 0x0A0;
-    static const PROG_ROM_RAM_BEGIN: u16 = 0x200;
-    static const PROG_ROM_RAM_END:   u16 = 0xFFF;
+    opcode: u16,
+    stack:  [u16; STACK_SIZE],
+    sp:     u8,  // stack pointer
+    render_out: Vec<u8>,  // should have RENDER_WIDTH * RENDER_HEIGHT elements, 64x32 B&W display
 
-    let memory: [u8; MEMORY_BYTES];
-    let v:      [u8; REGISTER_COUNT];  // registers V1-VF
-    let i:           u16;              // index register
-    let pc:          u16;              // program counter
-    let delay_timer: u8;               // decrement if not 0
-    let sound_timer: u8;               // decrement if not 0, beep on reaching 0
-    let key_states:  [u8; KEY_COUNT];  // hex keypad buttonstates
+    draw_flag: bool,  // whether or not a GUI application should render
+}
 
-    let opcode: u16;
-    let stack:  [u16; STACK_SIZE];
-    let sp:     u8;  // stack pointer
-    let render_out: [u8; RENDER_WIDTH * RENDER_HEIGHT];  // 64x32 B&W display
 
-    let draw_flag: bool;  // whether or not a GUI application should render
-};
+impl Chip8 {
+
+    fn new () -> Chip8 {
+        // TODO
+        // call init
+    }
+
+    fn init (&self, rand_seed: u32) {
+        // memory MEMORY_BYTES
+        for &byte in self.memory.iter() {
+            byte = 0u8;
+        }
+        self.i  = 0x0000;
+        self.pc = PROG_ROM_RAM_BEGIN;
+        self.delay_timer = 0;
+        self.sound_timer = 0;
+    }
+
+
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let c8 = Chip8::new();
 }
