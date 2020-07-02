4d7185e7777f49f03f5ce4784862633af42c82f5
Chris Barrett <chris@codesaru.com>
2020-07-01 18:59:17 -0700

    Working SDL2 crate demo.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@
 
 use rand::Rng;
 
+extern crate sdl2;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+//use sdl2::time::Duration;
+
 
 const MEMORY_BYTES:   usize = 4096;
 const REGISTER_COUNT: usize = 16;
@@ -234,6 +240,42 @@ fn main() {
              c8.memory[PROG_ROM_RAM_BEGIN as usize + 2],
              c8.memory[PROG_ROM_RAM_BEGIN as usize + 3]);
 
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("CHIP-8", 640, 320)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(std::time::Duration::from_millis(16));
+    }
+
+    /*/
     loop {
         match c8.emulate_cycle() {
             Ok(_) => continue,
@@ -243,5 +285,6 @@ fn main() {
             },
         }
     }
+    // */
 }
 
